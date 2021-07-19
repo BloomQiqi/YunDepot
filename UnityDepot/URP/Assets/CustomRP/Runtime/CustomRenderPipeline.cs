0597943e7ea745178cb3ae2ba51b951191300b54
qiangqi <qiangqi@pwrd.com>
2021-07-19 14:04:49 +0800

    添加Unity URP工程

---
+++ UnityDepot/URP/Assets/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    CameraRenderer renderer = new CameraRenderer();
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        //后续可支持多相机使用不同的Renderer
+        foreach(var camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+}
