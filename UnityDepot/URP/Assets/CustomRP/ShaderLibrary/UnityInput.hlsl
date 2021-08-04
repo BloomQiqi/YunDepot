a924ce795e240f9aa6c08075039fd986ea93b907
qiangqi <qiangqi@pwrd.com>
2021-08-04 22:07:47 +0800

    8.4

--- UnityDepot/URP/Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
+++ UnityDepot/URP/Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
@@ -4,7 +4,7 @@
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl" //1
 
 CBUFFER_START(UnityPerDraw)   //CBUFFER_SRART CBUFFER_END 是在上面的1库中定义的
-	float4x4 unity_ObjectToWorld;
+	float4x4 unity_ObjectToWorld;//内置
 	float4x4 unity_WorldToObject;
 	float4 unity_LODFade;
 	real4 unity_WorldTransformParams;
